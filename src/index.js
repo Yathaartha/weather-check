6562a2c649fa99f8d6e092b26254aec84878c885
Yathaartha <maharjanyathaartha017@gmail.com>
2021-05-09 13:46:12 +0545

    lifecycle methods

--- src/index.js
+++ src/index.js
@@ -5,24 +5,29 @@ class App extends Component {
   constructor(props) {
     super(props);
 
+    // THIS IS THE ONLY TIME we do direct assignment to this.state
     this.state = {
       //the state object
       lat: null,
+      errorMessage: "",
     };
+  }
 
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
-      (position) => {
-        // we called setState!!!
-        this.setState({ lat: position.coords.latitude });
-      },
-      (err) => {
-        console.log(err);
-      }
+      (position) => this.setState({ lat: position.coords.latitude }),
+      (err) => this.setState({ errorMessage: err.message })
     );
   }
 
   render() {
-    return <div>Latitude: {this.state.lat}</div>;
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage} </div>;
+    } else if (!this.state.errorMessage && this.state.lat) {
+      return <div>Latitude: {this.state.lat}</div>;
+    } else {
+      return <div>Loading...</div>;
+    }
   }
 }
 
